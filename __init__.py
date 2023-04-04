061cef9a0f8016794ee57042386fee49c15aae5d
Tanmay Deshpande <deshpandetanmay365@gmail.com>
2023-04-04 16:33:15 +0530

    initial commit

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""here we make the program"""
+def main():
+
+    """the main function
+    this is imported in the __main__ file
+    the __main__ file is the one that is run when we run it using python -m"""
+
+    print("hello world")
---
+++ __main__.py
@@ -0,0 +1,6 @@
+"""here we write the run code
+the program is run by the command python -m pinball
+this way pretending our module is an application is easier"""
+from . import main
+if __name__ == "__main__":
+    main()
